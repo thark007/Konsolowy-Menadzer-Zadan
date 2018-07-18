bcfad6421d4ea9fab9f894ae652049e2e6534492
thark007 <40567712+thark007@users.noreply.github.com>
2018-07-18 20:16:53 +0200

    TaskModel added

---
+++ Workshop_1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Workshop_1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string Task = "";
+			Console.WriteLine("Wpisz co chcesz zrobić: ");
+			Console.WriteLine("wpisz \'dodaj zadanie\' aby dodać zadanie");
+			Console.WriteLine("wpisz \'usuń zadanie\' aby usunąć zadanie");
+			Console.WriteLine("wpisz \'zapisz zadanie\' aby zapisać zadanie");
+			Console.WriteLine("wpisz \'wczytaj zadania\' aby wczytać zadania");
+			Console.WriteLine("wpisz \'exit\' aby wyjść z programu");
+			do
+			{
+				
+
+				Task = Console.ReadLine();
+				//TaskModel taskModel = new TaskModel();
+
+				if (Task == "Dodaj zadanie".ToLower())
+				{
+					//taskModel.AddTask();
+					//TaskModel taskModel = new TaskModel();
+					//Console.WriteLine(taskModebl.AddTask(Task));
+				}
+
+				if (Task == "Skasuj zadanie")
+				{
+				}
+				/*Console.WriteLine("które zadanie chcesz usunąć:");
+				string zadanie = Console.ReadLine();
+				taskList.RemoveAt(0);
+			}
+
+			if (Task == "Pokaż zadania")
+			{
+				foreach (TaskModel taskModel in taskList)
+				{
+					taskModel.ShowTasks();
+				}
+			}*/
+
+			} while (Task != "exit");
+		}
+	}
+}
+
+
+
+
---
+++ Workshop_1/TaskModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Workshop_1
+{
+	public class TaskModel
+	{
+		string _discription;
+		DateTime _taskStart;
+		DateTime _taskEnd;
+		Nullable<bool> _isTaskImportawnt;
+		Nullable<bool> _isTaskFullDay;
+
+
+		List<string[]> taskModel = new List<string[]>();
+
+		public TaskModel(string discription, DateTime taskStart, DateTime taskEnd, Nullable<bool> isTaskImportant, Nullable<bool> isTaskFullDay)
+		{
+			_discription = discription;
+			_taskStart = taskStart;
+			_taskEnd = taskEnd;
+			_isTaskImportawnt = isTaskImportant;
+			_isTaskFullDay = isTaskFullDay;
+		}
+
+		public void AddTask()
+		{
+			
+		}
+	}
+}
+
+
+
+/* 			c[0] = comment;
+File.WriteAllLines(path, c);		
+		Console.WriteLine("Zadanie zostało zapisane ");
+
+		return comment;
+		public TaskModel
+		{
+		taskList = new List<string>();
+		}
+
+		<float> taskList
+	public string Zadanie { get; private set; }
+
+string path = @"C:\Tasks\text.txt";
+string[] c;
+
+//		public TaskModel(string zadanie)
+//		{
+//			Zadanie = zadanie;
+//		}
+
+
+public void AddTask()
+{
+	Console.WriteLine("napisz treść zadania:");
+	string zadanie = Console.ReadLine();
+}
+
+
+
+public void ShowTasks()
+{
+	Console.WriteLine($"{Zadanie}");*/
